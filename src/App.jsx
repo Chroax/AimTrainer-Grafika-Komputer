2dd100de2f7380b74f17b04fbe8359c2f2de499c
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-12-03 10:23:36 +0700

    add logic for app in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,61 @@
 import { useEffect } from 'react';
 import * as THREE from 'three';
 
+let GAME_STATE = "IDLE";
+let MAX_TIME = prompt("MAX_TIME (s)");
+let START_TIME = null;
+let HIGH_SCORE = localStorage.getItem("highscore") ? localStorage.getItem("highscore") : 0;
+let THIS_ACTIVE = true;
+let CLICKABLE_OBJ = [];
+let SPHERE_RADIUS = 5;
+let MAX_TARGET = 3;
+let score = 0;
+
+let boundary = {
+  x: { min: -20, max: 20 },
+  y: { min: -20, max: 20 },
+  z: { min: -100, max: -8 },
+}
+
+let light_objects = {
+  DirectionalLight: {
+    active: true,
+    members: []
+  },
+  HemisphereLight: {
+    active: false,
+    members: []
+  },
+  AmbientLight: {
+    active: false,
+    members: []
+  },
+  PointLight: {
+    active: false,
+    members: []
+  },
+  Spotlights: {
+    active: false,
+    members: []
+  }
+};
+
 function App() {
   useEffect(() => {
 
+    let currentRunningProgram = null;
+
+    document.querySelector('#start-button').addEventListener('click', (event) => {
+      document.querySelector('#mainmenu').style.display = 'none';
+      document.querySelector('#crosshair').style.display = 'flex';
+      document.querySelector('#score-container').style.display = 'flex';
+      document.querySelector('#time-container').style.display = 'flex';
+      document.body.requestPointerLock();
+      GAME_STATE = "PLAY";
+      START_TIME = new Date();
+      document.querySelector('#score').innerHTML = 0;
+      currentRunningProgram = animate();
+    })
   }, []);
 
   return (
@@ -13,4 +65,320 @@ function App() {
   )
 }
 
+function animate() {
+  const fontloader = new THREE.FontLoader();
+
+  const fov = 90;
+  const aspect = window.innerWidth / window.innerHeight;
+  const near = 0.1;
+  const far = 1000;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 0, 50);
+
+  let canvas = document.querySelector('#canvas');
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    logarithmicDepthBuffer: true,
+    antialias: true,
+    powerPreference: "high-performance",
+    alpha: false
+  });
+
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  const raycaster = new THREE.Raycaster();
+
+  let lightBarProps = {
+    DirectionalLight: false,
+    HemisphereLight: false,
+    AmbientLight: false,
+    PointLight: true,
+    Spotlights: false,
+  }
+
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x21252d);
+  document.body.addEventListener('mousemove', (event) => {
+    if (document.pointerLockElement === document.body) {
+      camera.rotation.y -= event.movementX / 500;
+      camera.rotation.x -= event.movementY / 500;
+    }
+
+  });
+
+  {
+    light_objects.DirectionalLight.members.push(DirectionalFactory(0xFFFFFF, 0.3, [-25, 50, 25]));
+    light_objects.DirectionalLight.members.push(DirectionalFactory(0xFFFFFF, 0.3, [25, 50, 25]));
+    light_objects.DirectionalLight.members.push(DirectionalFactory(0xFFFFFF, 0.3, [-25, 50, -25]));
+    light_objects.DirectionalLight.members.push(DirectionalFactory(0xFFFFFF, 0.3, [25, 50, -25]));
+
+    light_objects.DirectionalLight.members.push(DirectionalFactory(0xFFFFFF, 0.3, [-30, 0, 30]));
+    light_objects.DirectionalLight.members.push(DirectionalFactory(0xFFFFFF, 0.3, [30, 0, 30]));
+    light_objects.DirectionalLight.members.push(DirectionalFactory(0xFFFFFF, 0.3, [-30, 0, -30]));
+    light_objects.DirectionalLight.members.push(DirectionalFactory(0xFFFFFF, 0.3, [30, 0, -30]));
+  }
+
+  {
+    const skyColor = generateRandomColor();  // light blue
+    const groundColor = generateRandomColor();  // brownish orange
+    const intensity = 1;
+    const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+
+    light_objects.HemisphereLight.members.push(light);
+  }
+
+  {
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.AmbientLight(color, intensity);
+    light_objects.AmbientLight.members.push(light);
+  }
+
+  {
+    light_objects.PointLight.members.push(PointLightFactory(0xffffff, 1, [-25, 50, 25]));
+    light_objects.PointLight.members.push(PointLightFactory(0xffffff, 1, [25, 50, 25]));
+    light_objects.PointLight.members.push(PointLightFactory(0xffffff, 1, [-25, 50, -25]));
+    light_objects.PointLight.members.push(PointLightFactory(0xffffff, 1, [25, 50, -25]));
+
+    light_objects.PointLight.members.push(PointLightFactory(generateRandomColor(), 1, [-30, 0, 30]));
+    light_objects.PointLight.members.push(PointLightFactory(generateRandomColor(), 1, [30, 0, 30]));
+    light_objects.PointLight.members.push(PointLightFactory(generateRandomColor(), 1, [-30, 0, -30]));
+    light_objects.PointLight.members.push(PointLightFactory(generateRandomColor(), 1, [30, 0, -30]));
+  }
+
+  {
+    light_objects.Spotlights.members.push(SpotLightFactory(generateRandomColor(), 1, [-25, 50, 25], [0, 0, 0]));
+    light_objects.Spotlights.members.push(SpotLightFactory(generateRandomColor(), 1, [25, 50, 25], [0, 0, 0]));
+  }
+
+  setLight('PointLight', true);
+
+  const axesHelper = new THREE.AxesHelper(40);
+  scene.add(axesHelper);
+
+  let max_color = 10;
+  let color_list = [];
+
+  for (let i = 0; i < max_color; i++) {
+    color_list.push({ color: generateRandomColor(), displayed: 0 });
+  }
+
+  for (let i = 0; i < MAX_TARGET; i++) {
+    addBall();
+  }
+
+  document.body.addEventListener("click", (event) => {
+    if (!THIS_ACTIVE)
+      return;
+
+    if (event.which == 3) {
+      selected_object.forEach((item) => {
+        scene.remove(item.item.wireframe);
+      });
+    }
+
+    document.body.requestPointerLock()
+
+    let middle_point = new THREE.Vector2(0, 0);
+    raycaster.setFromCamera(middle_point, camera);
+    var intersects = raycaster.intersectObjects(scene.children); //array
+
+    let objectTerklik = false;
+    intersects.forEach((obj) => {
+      CLICKABLE_OBJ.forEach((C_OBJ) => {
+        if (C_OBJ.item.solid == obj.object) {
+          selectObject(C_OBJ);
+          objectTerklik = true;
+          const audio_click = new Audio('./roblox_death.mp3');
+          audio_click.play();
+        }
+      })
+    });
+
+    if (!objectTerklik) {
+      score -= 10;
+      document.querySelector('#score').innerHTML = score;
+      const audio_wiff = new Audio('./bruh.mp3');
+      audio_wiff.play();
+    }
+  }, true);
+
+  render();
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '0x';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return parseInt(color, 16);
+}
+
+function updateCamera() {
+  camera.updateProjectionMatrix();
+}
+
+function setLight(type, active) {
+  if (active) {
+    light_objects[type].members.forEach(light => scene.add(light));
+  }
+  else {
+    light_objects[type].members.forEach(light => scene.remove(light));
+    light_objects[type].active = false;
+  }
+}
+
+function DirectionalFactory(color, intensity, position) {
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(position[0], position[1], position[2]);
+  return light;
+}
+
+function PointLightFactory(color, intensity, position) {
+  const light = new THREE.PointLight(color, intensity);
+  light.position.set(position[0], position[1], position[2]);
+  return light;
+}
+
+function SpotLightFactory(color, intensity, position, target_pos) {
+  const light = new THREE.SpotLight(color, intensity);
+  light.position.set(position[0], position[1], position[2]);
+  light.target.position.set(position[0], position[1], position[2]);
+  return light;
+}
+
+function ballFactory(color) {
+  const obj_geometry = new THREE.SphereGeometry(SPHERE_RADIUS);
+  const obj_material = new THREE.MeshPhongMaterial({ color: color, shininess: 150 });
+
+  const wireframe_geometry = new THREE.WireframeGeometry(obj_geometry);
+  const wireframe_material = new THREE.LineBasicMaterial({ color: 0xffffff });
+
+  let obj = {}
+  obj.solid = new THREE.Mesh(obj_geometry, obj_material);
+  obj.wireframe = new THREE.LineSegments(wireframe_geometry, wireframe_material);
+
+  return obj;
+}
+
+function addBall() {
+  let color = 0xff2222;
+
+  let new_position = null;
+
+  while (true) {
+    new_position = [getRndInteger(boundary.x.min, boundary.x.max), getRndInteger(boundary.y.min, boundary.y.max), 0];
+    let isOK = true;
+
+    for (let i = 0; i < CLICKABLE_OBJ.length; i++) {
+      if (Math.abs(CLICKABLE_OBJ[i].item.solid.position.x - new_position[0]) <= SPHERE_RADIUS || Math.abs(CLICKABLE_OBJ[i].item.solid.position.y - new_position[1]) <= SPHERE_RADIUS) {
+        isOK = false;
+        break;
+      }
+    }
+
+    if (isOK) {
+      break;
+    }
+  }
+
+  let position = new_position;
+  let obj = ballFactory(color);
+  obj.solid.position.set(position[0], position[1], position[2]);
+  obj.wireframe.position.set(position[0], position[1], position[2]);
+
+  CLICKABLE_OBJ.push({
+    item: obj,
+    color: color
+  });
+
+  scene.add(obj.solid);
+  return obj;
+}
+
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
+function selectObject(object) {
+  let new_position = null;
+  while (true) {
+    new_position = [getRndInteger(boundary.x.min, boundary.x.max), getRndInteger(boundary.y.min, boundary.y.max), 0];
+    let isOK = true;
+
+    for (let i = 0; i < CLICKABLE_OBJ.length; i++) {
+      if (CLICKABLE_OBJ[i] == object) continue;
+
+      if (Math.abs(CLICKABLE_OBJ[i].item.solid.position.x - new_position[0]) <= SPHERE_RADIUS || Math.abs(CLICKABLE_OBJ[i].item.solid.position.y - new_position[1]) <= SPHERE_RADIUS) {
+        isOK = false;
+        break;
+      }
+    }
+
+    if (isOK) {
+      break;
+    }
+  }
+
+  let position = new_position;
+
+  object.item.solid.position.set(position[0], position[1], position[2]);
+  score += 100;
+  document.querySelector('#score').innerHTML = score;
+}
+
+function render() {
+  let currentTime = new Date();
+  var timeDiff = currentTime - START_TIME;
+  timeDiff /= 1000;
+
+  var seconds = Math.round(timeDiff);
+  if (seconds >= MAX_TIME) {
+
+    if (score > HIGH_SCORE) {
+      HIGH_SCORE = score;
+    }
+
+    GAME_STATE = "IDLE";
+    document.querySelector('#mainmenu').style.display = 'flex';
+    document.querySelector('#crosshair').style.display = 'none';
+    document.querySelector('#score-container').style.display = 'none';
+    document.querySelector('#time-container').style.display = 'none';
+    document.exitPointerLock();
+    scene.clear();
+    THIS_ACTIVE = false;
+
+    localStorage.setItem("highscore", HIGH_SCORE);
+    document.querySelector('#highscore').innerHTML = localStorage.getItem("highscore");
+    return;
+  }
+
+  document.querySelector('#time').innerHTML = MAX_TIME - seconds;
+
+  if (resizeRendererToDisplaySize(renderer)) {
+    console.log("RESIZED")
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+
+  renderer.render(scene, camera);
+  if (GAME_STATE != "IDLE") {
+    requestAnimationFrame(render);
+  }
+}
 export default App
