e67a6d6658ed3d5e514a3f1ab12f4675c2b2590e
soretaka <yatesthap@gmail.com>
2022-12-17 18:10:55 +0700

    added accuracy

--- src/Variable.js
+++ src/Variable.js
@@ -1,7 +1,9 @@
 let GAME_STATE = "IDLE";
 let MAX_TIME = prompt("MAX_TIME (s)");
 let START_TIME = null;
-let HIGH_SCORE = localStorage.getItem("highscore") ? localStorage.getItem("highscore") : 0;
+let HIGH_SCORE = localStorage.getItem("highscore")
+  ? localStorage.getItem("highscore")
+  : 0;
 const AUDIO_CLICK_RIGHT = "/src/assets/roblox_death.mp3";
 const AUDIO_CLICK_WRONG = "/src/assets/bruh.mp3";
 let THIS_ACTIVE = true;
@@ -14,4 +16,7 @@ let boundary = {
   x: { min: -20, max: 20 },
   y: { min: -20, max: 20 },
   z: { min: -100, max: -8 },
-}
+};
+let hit = 0;
+let click = 0;
+let accuracy = 100;
--- src/lib/EventListener.js
+++ src/lib/EventListener.js
@@ -1,9 +1,9 @@
 import Utility from "./Utility";
-import * as THREE from 'three';
+import * as THREE from "three";
 
 export default class EventListener {
   addMouseMove(camera) {
-    document.body.addEventListener('mousemove', (event) => {
+    document.body.addEventListener("mousemove", (event) => {
       if (document.pointerLockElement === document.body) {
         camera.rotation.y -= event.movementX / 500;
         camera.rotation.x -= event.movementY / 500;
@@ -13,38 +13,48 @@ export default class EventListener {
 
   addMouseClickListener(camera, scene, raycaster) {
     const utility = new Utility();
-    document.body.addEventListener("click", (event) => {
-      if (!THIS_ACTIVE) return;
-      if (event.which == 3) {
-        selected_object.forEach((item) => {
-          scene.remove(item.item.wireframe);
+    document.body.addEventListener(
+      "click",
+      (event) => {
+        if (!THIS_ACTIVE) return;
+        if (event.which == 3) {
+          selected_object.forEach((item) => {
+            scene.remove(item.item.wireframe);
+          });
+        }
+        document.body.requestPointerLock();
+
+        let middle_point = new THREE.Vector2(0, 0);
+        raycaster.setFromCamera(middle_point, camera);
+        var intersects = raycaster.intersectObjects(scene.children); //array
+        click += 1;
+        let objectTerklik = false;
+        intersects.forEach((obj) => {
+          CLICKABLE_OBJ.forEach((C_OBJ) => {
+            if (C_OBJ.item.solid == obj.object) {
+              utility.selectObject(C_OBJ);
+              score += 100;
+              hit += 1;
+              // console.log(hit);
+              // console.log(click);
+              document.querySelector("#score").innerHTML = score;
+              objectTerklik = true;
+              const audio_click = new Audio(AUDIO_CLICK_RIGHT);
+              audio_click.play();
+            }
+          });
         });
-      }
-      document.body.requestPointerLock()
-
-      let middle_point = new THREE.Vector2(0, 0);
-      raycaster.setFromCamera(middle_point, camera);
-      var intersects = raycaster.intersectObjects(scene.children); //array
-
-      let objectTerklik = false;
-      intersects.forEach((obj) => {
-        CLICKABLE_OBJ.forEach((C_OBJ) => {
-          if (C_OBJ.item.solid == obj.object) {
-            utility.selectObject(C_OBJ);
-            score += 100;
-            document.querySelector('#score').innerHTML = score;
-            objectTerklik = true;
-            const audio_click = new Audio(AUDIO_CLICK_RIGHT);
-            audio_click.play();
-          }
-        })
-      });
-      if (!objectTerklik) {
-        score -= 10;
-        document.querySelector('#score').innerHTML = score;
-        const audio_wiff = new Audio(AUDIO_CLICK_WRONG);
-        audio_wiff.play();
-      }
-    }, true);
+        if (!objectTerklik) {
+          score -= 10;
+          // console.log(click);
+          document.querySelector("#score").innerHTML = score;
+          const audio_wiff = new Audio(AUDIO_CLICK_WRONG);
+          audio_wiff.play();
+        }
+        accuracy = (hit / click) * 100;
+        console.log(accuracy);
+      },
+      true
+    );
   }
 }
